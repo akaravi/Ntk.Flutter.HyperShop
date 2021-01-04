9a9eb24fd4877484508d3a18819530232b5516e8
ML <34806399+akaravi@users.noreply.github.com>
2021-01-04 10:49:50 +0330

    Ntk Build

--- NtkCmsBuild.bat
+++ NtkCmsBuild.bat
@@ -2,7 +2,7 @@ echo Android Application Builder ver 1.0 , By NTK
 echo Strting Builing Application ....  
 echo. 
 rem flutter build appbundle
-flutter build
+flutter build apk --split-per-abi
 rem explorer "app\build\outputs\apk"
 rem echo. 
 set /p DUMMY=Hit ENTER to continue...
